583fa8a8f2e213257c2bf10084e767a53cc153d5
Duhan <dcms2@cin.ufpe.br>
2013-12-10 16:58:27 -0300

    Exponenciacao implementado

--- calculadora.cpp
+++ calculadora.cpp
@@ -5,9 +5,22 @@ using namespace std;
 class Calculadora {
 public:
     Calculadora() {}
+
+    int exponencia(int a, int b) {
+        int retorno = 1;
+        while (b) {
+            if (b&1) {
+                retorno *= a;
+            }
+            a *= a;
+            b >>= 1;
+        }
+        return retorno;
+    }
 };
 
-
 int main() {
+    Calculadora c = Calculadora();
+    printf("%d\n", c.exponencia(3,5));
     return 0;
 }
