d6f46f0e7568a8369aba5f38e3fa9e9f9fe198d8
Lucas Lima <lapl@cin.ufpe.br>
2013-12-10 16:35:26 -0300

    add implementado

--- calculadora.cpp
+++ calculadora.cpp
@@ -5,9 +5,15 @@ using namespace std;
 class Calculadora {
 public:
     Calculadora() {}
+    
+    int add(int a, int b) {
+        return a + b;
+    }
 };
 
 
 int main() {
+    Calculadora c = Calculadora();
+    cout << c.add(4,5) << endl;
     return 0;
 }
